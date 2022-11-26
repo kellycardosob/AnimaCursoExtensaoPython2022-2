9dbdc0dc37a52421dba5816d030633be06c301a5
kelly barbosa <95001079+kellycardosob@users.noreply.github.com>
2022-11-26 00:32:37 +0000

    Comando de entrada input() e a diferença de string para int

---
+++ aula1_19_10.py
@@ -0,0 +1,21 @@
+# Meu primeiro projeto Python!!!
+#
+# print() = comando de saida
+print("Alo mundo!")
+
+# Quando quiser guardar uma String! (Frase)
+
+nome ="Kelly Cardoso"
+# Quando quiser guardar um numero inteiro
+idade = 18
+# Exibir o meu nome ( que esta dentro da variavel nome)
+print(nome)
+
+# Quando quiser exibir a frase "MInha idade é " completando com o conteudo da varaivel idade
+print("Minha idade é "+str(idade)+" anos")
+print(f"Minha idade é {idade} anos")
+print("Minha idade é {} anos".format(idade))
+
+#Quando quiser exibir "Meu nome é ... e tenho ... 
+#anos.." trocando pelas variáveis nome e idade
+print("Meu nome é {} e tenho {} anos".format(nome, idade))
